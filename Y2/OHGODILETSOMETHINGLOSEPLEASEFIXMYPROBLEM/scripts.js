47b7b143ec7c7eb6916c17ed1b453b8390fa6185
Connerkirkley <141285547+Connerkirkley@users.noreply.github.com>
2024-09-09 15:20:28 -0700

    dtyfgubjh
    ftyghivbujn

--- Y2/OHGODILETSOMETHINGLOSEPLEASEFIXMYPROBLEM/scripts.js
+++ Y2/OHGODILETSOMETHINGLOSEPLEASEFIXMYPROBLEM/scripts.js
@@ -1,1 +1,30 @@
-console.log(1 * 8 * 1024 * 1024 * 1024);
+const http = require('http');
+const fs = require('fs');
+
+
+
+const puzzles = fs.readFileSync('./html/puzzle10.html', 'utf8');
+
+console.log(puzzles);
+
+const server = http.createServer((req, res) => {
+    
+    if(req.url === '/'){
+        res.end("WELCOME TO THE JUNGLE WHERE IT'S ALL FUN AND GAMES")
+    }
+    else if(req.url === '/about'){
+        res.end('nuh uh')
+    }
+    else{
+        res.end('NUH UH')
+    }
+})
+
+
+
+
+
+
+
+
+
